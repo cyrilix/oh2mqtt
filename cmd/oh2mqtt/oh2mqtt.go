c84458126bfcce5f43a35cf24aaf0ac57d262f0c
Cyrille Nofficial <cynoffic@cyrilix.fr>
2022-12-04 15:06:52 +0100

    fix: bad mqtt tls configuration

--- cmd/oh2mqtt/oh2mqtt.go
+++ cmd/oh2mqtt/oh2mqtt.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/cyrilix/oh2mqtt/pkg/cli"
+	"github.com/cyrilix/mqtt-tools/mqttTooling"
 	"github.com/cyrilix/oh2mqtt/pkg/events"
 	"github.com/hellofresh/health-go/v4"
 	"go.uber.org/zap"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,17 +20,28 @@ const (
 	DefaultClientId = "openhome2mqtt"
 )
 
+func HandleExit(c io.Closer) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Kill, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		_ = c.Close()
+		os.Exit(0)
+	}()
+}
+
 func main() {
 
-	var mqttBroker, username, password, clientId string
 	var intervalPublishSec int
 	var topicBaseName string
 	var probeListen, probePath string
 
-	mqttQos := cli.InitIntFlag("MQTT_QOS", 0)
-	_, mqttRetain := os.LookupEnv("MQTT_RETAIN")
+	parameters := mqttTooling.MqttCliParameters{
+		ClientId: DefaultClientId,
+	}
 
-	cli.InitMqttFlags(DefaultClientId, &mqttBroker, &username, &password, &clientId, &mqttQos, &mqttRetain)
+	mqttTooling.InitMqttFlagSet(&parameters)
 
 	flag.IntVar(&intervalPublishSec, "interval-time-pub", 10, "time interval inb seconds between 2 msgs")
 	flag.StringVar(&topicBaseName, "topic-base-name", "openhome/room/%s/%s", "Topcic name pattern to publish")
@@ -55,7 +69,7 @@ func main() {
 	}()
 	zap.ReplaceGlobals(lgr)
 
-	client, err := cli.Connect(mqttBroker, username, password, clientId)
+	client, err := mqttTooling.Connect(&parameters)
 	if err != nil {
 		zap.S().Fatalf("unable to connect to mqtt bus: %v", err)
 	}
@@ -65,7 +79,7 @@ func main() {
 	ohg := events.NewOHGateway(p, events.WithIntervalPublish(time.Duration(intervalPublishSec)*time.Second), events.WithTopicFormat(topicBaseName))
 	defer func() { _ = ohg.Close() }()
 
-	cli.HandleExit(ohg)
+	HandleExit(ohg)
 
 	healthz, _ := health.New(
 		health.WithChecks(health.Config{
